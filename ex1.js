fb81b8b7801688ea88f61f0f3a40fb2b9e5d8c7a
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2023-12-17 19:45:28 +0500

    commit after moving JS files to dist folder

--- ex1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Exercise 1: Write a program that calculates the area of a rectange
-Object.defineProperty(exports, "__esModule", { value: true });
-const lengthOfRectangle = 30;
-const widthOfRectangle = 15;
-const areaOfRectangle = lengthOfRectangle * widthOfRectangle;
-console.log(`The area of the rectangle is ${areaOfRectangle} cm`);
