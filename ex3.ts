c2da3aa9af2739cb36b1e25757551abfe86dbace
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 15:43:27 +0500

    program updated using function approach

--- ex3.ts
+++ ex3.ts
@@ -1,13 +1,15 @@
 // Exercise 3: Write a program that checks if a given number is positive, negative, or zero.
 
-const num = 0;
-
-if (num > 0) {
-  console.log("Positive");
-} else if (num < 0) {
-  console.log("Negative");
-} else {
-  console.log("Zero");
+function checkNumberSign(userInput: number) {
+  if (userInput > 0) {
+    console.log("The number is positive");
+  } else if (userInput < 0) {
+    console.log("The number is negative");
+  } else {
+    console.log("The number is zero");
+  }
 }
 
-export {};
+checkNumberSign(10);
+checkNumberSign(-5);
+checkNumberSign(0);
