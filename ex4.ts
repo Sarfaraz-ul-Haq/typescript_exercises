e958617134337454f4b176fd374fe4b33b83a776
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 15:48:46 +0500

    program updated using function approach

--- ex4.ts
+++ ex4.ts
@@ -1,11 +1,12 @@
 // Exercise 4: Write a program that checks if a given number is even or odd.
 
-const num = 6;
-
-if (num % 2 == 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
+function isEvenOrOdd(userInput: number) {
+  if (userInput % 2 == 0) {
+    console.log("The number is even");
+  } else {
+    console.log("The number is odd");
+  }
 }
 
-export {};
+isEvenOrOdd(2); // prints "The number is even"
+isEvenOrOdd(5); // prints "The number is odd"
