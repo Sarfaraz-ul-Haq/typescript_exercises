58429ebe0ec81a98038741e026d5d1ad69ec09c8
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 15:52:04 +0500

    program updated using function approach

--- ex5.ts
+++ ex5.ts
@@ -1,11 +1,13 @@
 // Exercise 5: Write a program that determines if a person is eligible to vote based on their age.
 
-const personAge = 19;
-
-if (personAge > 18) {
-  console.log("You are eligible to vote");
-} else {
-  console.log("Age restriction");
+function isEligibleToVote(age: number): boolean {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-export {};
+isEligibleToVote(15); // false
+isEligibleToVote(18); // true
+isEligibleToVote(25); // true
